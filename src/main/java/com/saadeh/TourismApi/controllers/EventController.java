2e07b9cd35dd132a48a80fee81211697511d5254
Wilson Saadeh <wilson_saadeh@hotmail.com>
2026-02-03 14:36:51 -0300

    Crud using restcontroller

--- src/main/java/com/saadeh/TourismApi/controllers/EventController.java
+++ src/main/java/com/saadeh/TourismApi/controllers/EventController.java
@@ -5,9 +5,11 @@ import com.saadeh.TourismApi.services.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/events")
@@ -19,5 +21,33 @@ public class EventController {
     @GetMapping
     public Page<EventDto> findAll(Pageable pageable){
         return service.findAll(pageable);
+    }
+
+    @GetMapping(value = "/{id}")
+    public EventDto findById(@PathVariable Long id){
+        return service.findById(id);
+    }
+
+    @PostMapping
+    public ResponseEntity<EventDto> insert(@RequestBody EventDto dto){
+        EventDto result = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(dto.getId())
+                .toUri();
+        return ResponseEntity.created(uri).body(result);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<EventDto> update(@PathVariable Long id,@RequestBody EventDto dto){
+        dto = service.update(id,dto);
+        return ResponseEntity.ok().body(dto);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/saadeh/TourismApi/services/EventService.java
+++ src/main/java/com/saadeh/TourismApi/services/EventService.java
@@ -1,9 +1,15 @@
 package com.saadeh.TourismApi.services;
 
 import com.saadeh.TourismApi.dto.EventDto;
+import com.saadeh.TourismApi.entities.Country;
 import com.saadeh.TourismApi.entities.Event;
+import com.saadeh.TourismApi.repositories.CountryRepository;
 import com.saadeh.TourismApi.repositories.EventRepository;
+import com.saadeh.TourismApi.services.exceptions.DatabaseException;
+import com.saadeh.TourismApi.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -17,15 +23,65 @@ public class EventService {
     @Autowired
     private EventRepository repository;
 
+    @Autowired
+    private CountryRepository countryRepository;
+
     @Transactional(readOnly = true)
-    public Page<EventDto> findAll(Pageable pageable){
+    public Page<EventDto> findAll(Pageable pageable) {
         Page<Event> result = repository.findAll(pageable);
         return result.map(EventDto::new);
     }
 
     @Transactional(readOnly = true)
-    public EventDto findById(Long id){
-        Event result = repository.findById(id).get();
-        return new EventDto(result);
+    public EventDto findById(Long id) {
+        return repository.findById(id)
+                .stream()
+                .map(EventDto::new)
+                .findFirst()
+                .orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+    }
+
+    @Transactional
+    public EventDto insert(EventDto dto){
+        Event entity = new Event();
+        copyDtoToEntity(dto,entity);
+        entity = repository.save(entity);
+        return new EventDto(entity);
+    }
+
+    @Transactional
+    public EventDto update(Long id,EventDto dto){
+        try {
+            Event entity = repository.getReferenceById(id);
+            copyDtoToEntity(dto,entity);
+            entity = repository.save(entity);
+            return new EventDto(entity);
+        }catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException("Id not found " + id);
+        }
+    }
+
+    @Transactional
+    public void delete(Long id){
+        if (!repository.existsById(id)){
+            throw new ResourceNotFoundException("Id not found " + id);
+        }
+        try {
+            repository.deleteById(id);
+        }catch (DataIntegrityViolationException e){
+            throw new DatabaseException("Integrity violation");
+        }
+    }
+
+    private void copyDtoToEntity(EventDto dto, Event entity) {
+        entity.setTitle(dto.getTitle());
+        entity.setDescription(dto.getDescription());
+        entity.setJoker(dto.isJoker());
+        entity.setOk(dto.isOk());
+        Country c = countryRepository.findById(dto.getCountry().getId()).get();
+        if (!c.equals(null)) {
+            dto.setCountry(c);
+        }
+        entity.setCountry(dto.getCountry());
     }
 }
